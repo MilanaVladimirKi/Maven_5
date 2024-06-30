07a8e38898c57a5a5f8b4f6755098f3aa4eb8ca2
Milana Kichutkina <MilanaVladimir@mail.ru>
2024-06-30 23:28:38 +0500

    without setters and getters

--- src/main/java/ru/topacademy/javaqa/domain/Product.java
+++ src/main/java/ru/topacademy/javaqa/domain/Product.java
@@ -11,28 +11,12 @@ public class Product {
         this.price = price;
     }
 
-    public int getPrice() {
-        return price;
-    }
-
     public String getName() {
         return name;
     }
 
     public int getID() {
         return ID;
-    }
-
-    public void setID(int ID) {
-        this.ID = ID;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setPrice(int price) {
-        this.price = price;
     }
 }
 
